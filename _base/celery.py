822cbd191c95676abf02f43549f290dfad174521
kshyst <kiarash.sh1383@gmail.com>
2025-06-01 17:37:16 +0330

    changed celery settings for connecting to redis

--- _base/celery.py
+++ _base/celery.py
@@ -5,11 +5,9 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_base.settings')
 
 
-celery_app = Celery('deezcount', broker='redis://localhost:6379/0')
+celery_app = Celery('deezcount', broker=os.getenv('CELERY_BROKER_URL'))
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
-
-celery_app.broker_connection = 'redis://localhost:6379/0'
 
 celery_app.conf.beat_schedule = {
     'send-discounts-every-15-min': {
